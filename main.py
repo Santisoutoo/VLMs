8ee9935e27012d6a87d1ba10a75bc3e00c67dd2e
Santiago <149386030+Santisoutoo@users.noreply.github.com>
2025-11-14 12:54:13 +0100

    requirements

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-def main():
-    print("Hello from vlms!")
-
+import argparse
+from scr.models.model_1.blip_captioner import run_blip
+from scr.models.model_2.vitgpt2_captioner import run_vitgpt2
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", type=str, required=True)
+    args = parser.parse_args()
+
+    if args.model == "model_1":
+        run_blip()
+    elif args.model == "model_2":
+        run_vitgpt2()
+    else:
+        raise ValueError("Model must be: model_1 or model_2")
